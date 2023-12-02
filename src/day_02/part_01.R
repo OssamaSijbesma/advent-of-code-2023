30f2b6b60858b84ec08c932086ac5440f3f72846
Ossama Sybesma <ossama_sybesma@hotmail.com>
2023-12-02 14:29:17 +0000

    feat: Day 02

---
+++ src/day_02/part_01.R
@@ -0,0 +1,19 @@
+input <- readLines("input/day_02.txt") %>%
+  tibble(id = .) |>
+  separate_wider_delim(cols = id, delim = ":", names = c("id", "games")) |>
+  separate_longer_delim(games, ";") |>
+  separate_longer_delim(games, ",") |>
+  mutate(
+    id = str_extract(id, "\\d+") %>% as.integer(),
+    games = str_squish(games)
+  ) |>
+  separate_wider_delim(cols = games, delim = " ", names = c("result", "colour")) |>
+  mutate(result = as.integer(result)) %>%
+  summarise(.by = c(id, colour), max_result = max(result)) %>%
+  pivot_wider(id_cols = id, names_from = colour, values_from = max_result)
+
+print(
+  input %>%
+    filter(blue <= 14 & red <= 12 & green <= 13) %>%
+    summarize(sum(id))
+)
